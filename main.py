f70943878cca31652dbe15689844e2edeedba9b9
Adrian Paredes <adrianparedesroman99@gmail.com>
2025-03-25 18:01:36 -0400

    in progress json parsing

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from . import database, models, schemas
+from pydantic_core import from_json
 
 app =  FastAPI()
 
@@ -11,64 +12,86 @@ app =  FastAPI()
 load_dotenv()
 api_key = os.getenv("API_KEY")
 
-
 @app.get("/test")
 def testing():
-    return {"test": "success"}
-# r = requests.get(f'https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key={api_key}')
+    # prompt user for date range
+    # startDate = input("Enter a start date using this format YYYY-MM-DD: ")
+    # endDate = input("Enter an end date using this format YYYY-MM-DD: ")
+    
+    # r = requests.get(f'https://api.nasa.gov/neo/rest/v1/feed?start_date={startDate}&end_date={endDate}&api_key={api_key}')
+    r = requests.get(f'https://api.nasa.gov/neo/rest/v1/feed?start_date=2024-10-11&end_date=2024-10-11&api_key={api_key}')
+    print(r.status_code)  # check for any issues
+    # validation = from_json(r.json(), allow_partial=True)
+    # print(validation)
+    # validation = schemas.Neo.model_dump(**r.json(), exclude='links')
+    
+    print(schemas.NeoMetaData.model_validate_json(r.text))
+    return schemas.NeoMetaData.model_validate_json(r.text)
 
-# print(r.json())
+@app.get("/asteroids")
+def get_asteroids():
+    pass
 
+# current issue ---------------
+# Parsing json and only returning the information that matches the pydantic schema
 '''
+            "links": {
+        "next": "http://api.nasa.gov/neo/rest/v1/feed?start_date=2024-10-12&end_date=2024-10-12&detailed=false&api_key=0mT18MGDzJyEJ8ro9ocAPogkqkCHJvLLDXRc26e3",
+        "prev": "http://api.nasa.gov/neo/rest/v1/feed?start_date=2024-10-10&end_date=2024-10-10&detailed=false&api_key=0mT18MGDzJyEJ8ro9ocAPogkqkCHJvLLDXRc26e3",
+        "self": "http://api.nasa.gov/neo/rest/v1/feed?start_date=2024-10-11&end_date=2024-10-11&detailed=false&api_key=0mT18MGDzJyEJ8ro9ocAPogkqkCHJvLLDXRc26e3"
+    },
+    "element_count": 18,
+    "near_earth_objects": {
+        "2024-10-11": [
             {
                 "links": {
-                    "self": "http://api.nasa.gov/neo/rest/v1/neo/3709250?api_key=0mT18MGDzJyEJ8ro9ocAPogkqkCHJvLLDXRc26e3"
+                    "self": "http://api.nasa.gov/neo/rest/v1/neo/3639439?api_key=0mT18MGDzJyEJ8ro9ocAPogkqkCHJvLLDXRc26e3"
                 },
-                "id": "3709250",
-                "neo_reference_id": "3709250",
-                "name": "(2015 BY512)",
-                "nasa_jpl_url": "https://ssd.jpl.nasa.gov/tools/sbdb_lookup.html#/?sstr=3709250",
-                "absolute_magnitude_h": 21.49,
+                "id": "3639439",
+                "neo_reference_id": "3639439",
+                "name": "(2013 LB)",
+                "nasa_jpl_url": "https://ssd.jpl.nasa.gov/tools/sbdb_lookup.html#/?sstr=3639439",
+                "absolute_magnitude_h": 22.8,
                 "estimated_diameter": {
                     "kilometers": {
-                        "estimated_diameter_min": 0.1338304618,
-                        "estimated_diameter_max": 0.2992540101
+                        "estimated_diameter_min": 0.0732073989,
+                        "estimated_diameter_max": 0.1636967205
                     },
                     "meters": {
-                        "estimated_diameter_min": 133.8304618158,
-                        "estimated_diameter_max": 299.2540100804
+                        "estimated_diameter_min": 73.2073989347,
+                        "estimated_diameter_max": 163.696720474
                     },
                     "miles": {
-                        "estimated_diameter_min": 0.0831583679,
-                        "estimated_diameter_max": 0.1859477635
+                        "estimated_diameter_min": 0.0454889547,
+                        "estimated_diameter_max": 0.1017163949
                     },
                     "feet": {
-                        "estimated_diameter_min": 439.0763323438,
-                        "estimated_diameter_max": 981.8045264321
+                        "estimated_diameter_min": 240.181762721,
+                        "estimated_diameter_max": 537.0627483999
                     }
                 },
                 "is_potentially_hazardous_asteroid": false,
                 "close_approach_data": [
                     {
-                        "close_approach_date": "2025-03-10",
-                        "close_approach_date_full": "2025-Mar-10 12:23",
-                        "epoch_date_close_approach": 1741609380000,
+                        "close_approach_date": "2024-10-11",
+                        "close_approach_date_full": "2024-Oct-11 16:40",
+                        "epoch_date_close_approach": 1728664800000,
                         "relative_velocity": {
-                            "kilometers_per_second": "17.3279837746",
-                            "kilometers_per_hour": "62380.7415886568",
-                            "miles_per_hour": "38760.9669054629"
+                            "kilometers_per_second": "20.1270694374",
+                            "kilometers_per_hour": "72457.4499745792",
+                            "miles_per_hour": "45022.2416244823"
                         },
                         "miss_distance": {
-                            "astronomical": "0.4434335602",
-                            "lunar": "172.4956549178",
-                            "kilometers": "66336716.092436774",
-                            "miles": "41219723.9958405212"
+                            "astronomical": "0.3425269505",
+                            "lunar": "133.2429837445",
+                            "kilometers": "51241302.212395435",
+                            "miles": "31839868.760449603"
                         },
                         "orbiting_body": "Earth"
                     }
                 ],
                 "is_sentry_object": false
-            }
+            },
 
 
 
--- schemas.py
+++ schemas.py
@@ -1,28 +1,55 @@
+from typing import List, Dict
 from datetime import datetime
-from pydantic import BaseModel
+from . import main
+from pydantic import BaseModel, Field
 
-class MissDistance(BaseModel):
+class IgnoreExtraBase(BaseModel):
+    model_config = {"extra": "ignore"} #ignore any field that is not part of the model
+
+class MissDistance(IgnoreExtraBase):
     astronomical: float
     lunar: float
     kilometers: float
     miles: float
-class RelativeVelocity(BaseModel):
+class RelativeVelocity(IgnoreExtraBase):
     kilometers_per_second: float
     kilometers_per_hour: float
     miles_per_hour: float
-class ApproachData(BaseModel):
-    date: datetime
+class ApproachData(IgnoreExtraBase):
+    close_approach_date: datetime
     relative_velocity: RelativeVelocity
     miss_distance: MissDistance
     orbiting_body: str
-class NeoSize(BaseModel):
-    diameter_min: float
-    diameter_max: float
     
-class Neo(BaseModel):
+class DiameterF(IgnoreExtraBase):
+    estimated_diameter_min: float
+    estimated_dimater_max: float
+class DiameterMi(IgnoreExtraBase):
+    estimated_diameter_min: float
+    estimated_dimater_max: float
+class DiameterM(IgnoreExtraBase):
+    estimated_diameter_min: float
+    estimated_dimater_max: float
+class DiameterKil(IgnoreExtraBase):
+    estimated_diameter_min: float
+    estimated_dimater_max: float
+class NeoSize(IgnoreExtraBase):
+    kilometers: DiameterKil
+    meters: DiameterM
+    miles: DiameterMi
+    feet: DiameterF
+    
+class NeoCount(IgnoreExtraBase):
+    count: int
+    
+class Neo(IgnoreExtraBase):
     id: int
     name: str
-    size: NeoSize
-    hazardous: bool
-    approach_data: ApproachData
+    estimated_diameter: NeoSize
+    is_potentially_hazardous_asteroid: bool
+    close_approach_data: ApproachData 
     
+    
+class NeoMetaData(IgnoreExtraBase):
+    element_count: int
+    near_earth_objects: Dict[str, List[Neo]]
